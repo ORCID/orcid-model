5c626db63113515cd8f2e23acd1655529d6fc0c4
amontenegro <a.montenegro@orcid.org>
2024-01-05 13:46:06 -0600

    .

--- src/test/java/org/orcid/record/v3/common/PeerReviewSubjectTypesMatchesWorkTypesTest.java
+++ src/test/java/org/orcid/record/v3/common/PeerReviewSubjectTypesMatchesWorkTypesTest.java
@@ -5,17 +5,17 @@ import org.orcid.jaxb.model.common.PeerReviewSubjectType;
 import org.orcid.jaxb.model.common.WorkType;
 
 public class PeerReviewSubjectTypesMatchesWorkTypesTest {
-	
+
 	@Test
 	public void allWorkTypesHaveAPeerReviewSubjectTypeMatchTest() throws Exception {
 		WorkType workType = null;
-		try {			
-			for(WorkType w : WorkType.values()) {
+		try {
+			for (WorkType w : WorkType.values()) {
 				workType = w;
 				PeerReviewSubjectType.fromValue(w.value());
 			}
-		} catch(IllegalArgumentException e) {
-			throw new Exception ("Work type " + workType + " does not have a match in PeerReviewSubjectType enum");
+		} catch (IllegalArgumentException e) {
+			throw new Exception("Work type " + workType + " does not have a match in PeerReviewSubjectType enum");
 		}
 	}
 }
