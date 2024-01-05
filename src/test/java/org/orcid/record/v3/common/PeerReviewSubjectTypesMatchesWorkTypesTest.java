b33c2deb7cfb32a492ed7d4e4d4a20c3484f5112
amontenegro <a.montenegro@orcid.org>
2024-01-05 13:45:15 -0600

    Adding unit test

---
+++ src/test/java/org/orcid/record/v3/common/PeerReviewSubjectTypesMatchesWorkTypesTest.java
@@ -0,0 +1,21 @@
+package org.orcid.record.v3.common;
+
+import org.junit.Test;
+import org.orcid.jaxb.model.common.PeerReviewSubjectType;
+import org.orcid.jaxb.model.common.WorkType;
+
+public class PeerReviewSubjectTypesMatchesWorkTypesTest {
+	
+	@Test
+	public void allWorkTypesHaveAPeerReviewSubjectTypeMatchTest() throws Exception {
+		WorkType workType = null;
+		try {			
+			for(WorkType w : WorkType.values()) {
+				workType = w;
+				PeerReviewSubjectType.fromValue(w.value());
+			}
+		} catch(IllegalArgumentException e) {
+			throw new Exception ("Work type " + workType + " does not have a match in PeerReviewSubjectType enum");
+		}
+	}
+}
